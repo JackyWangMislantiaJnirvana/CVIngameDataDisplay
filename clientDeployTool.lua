c98565fa38172f4ea04882df9c448d543b595751
JackyWangMislantiaJnirvana <wmjwld@live.cn>
2019-06-15 20:21:33 +0800

    add client deploy script

---
+++ clientDeployTool.lua
@@ -0,0 +1,5 @@
+local os = require("os")
+
+os.execute("wget https://raw.githubusercontent.com/JackyWangMislantiaJnirvana/CVIngameDataDisplay/master/client/src/CVDDCentral.lua /home/CVDDCentral.lua")
+os.execute("wget https://raw.githubusercontent.com/JackyWangMislantiaJnirvana/CVIngameDataDisplay/master/client/lib/json.lua /usr/lib/json.lua")
+os.execute("wget https://raw.githubusercontent.com/JackyWangMislantiaJnirvana/CVIngameDataDisplay/master/client/lib/utils/separate.lua /usr/lib/utils/separate.lua")
