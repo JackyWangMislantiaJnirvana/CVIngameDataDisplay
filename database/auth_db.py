6fe49f7acd3d7951aa379c3a4b83c96d3040ff87
panda2134 <smallpanda2134@gmail.com>
2019-06-17 00:09:52 +0800

    settings work done. todo: cleanup directory in next commit !!! work on dashboard

--- database/auth_db.py
+++ database/auth_db.py
@@ -24,7 +24,8 @@ def append_invite_code() -> str:
 
     db.execute('INSERT INTO invite_code (code) values (?)', (code,))
     db.commit()
-    return code
+    cid = db.execute('SELECT * FROM invite_code where code = ?', (code,)).fetchone()['id']
+    return cid, code
 
 
 def remove_invite_code(code):
@@ -34,7 +35,11 @@ def remove_invite_code(code):
         db.commit()
 
 
+def get_invcode_list():
+    return database.get_database().execute('SELECT * FROM invite_code').fetchall()
+
 # User creation and login database
+
 
 def get_user(username):
     return database.get_database().execute('SELECT * FROM user where username = ?', (username,)).fetchone()
@@ -42,10 +47,6 @@ def get_user(username):
 
 def get_userlist():
     return database.get_database().execute('SELECT * FROM user').fetchall()
-
-
-def is_admin(username):
-    return get_user(username)['admin'] == 1
 
 
 def set_user_description(username, desc):
@@ -62,10 +63,24 @@ def generate_password_hash(password) -> str:
     return hashlib.sha1((password + current_app.config['SALT']).encode('utf-8')).hexdigest()
 
 
+def generate_strong_password() -> str:
+    return ''.join(random.sample(string.ascii_letters + string.digits, 12))
+
+
 def set_user_password(username, password):
     db = database.get_database()
     h = generate_password_hash(password)
     db.execute('UPDATE user SET password_hash=? WHERE username=?', (h, username))
+    db.commit()
+
+
+def is_admin(username):
+    return get_user(username)['admin'] == 1
+
+
+def set_admin(username: str, is_admin: bool):
+    db = database.get_database()
+    db.execute('UPDATE user SET admin=? WHERE username=?', (int(is_admin), username))
     db.commit()
 
 
@@ -79,13 +94,21 @@ def create_user(username, password, code, api_secret, admin=False):
     db.commit()
 
 
+def remove_user(username):
+    db = database.get_database()
+
+    assert username_exists(username)
+    db.execute('DELETE FROM user WHERE username = ?', (username,))
+    db.commit()
+
+
 def validate_login(username, password) -> bool:
     db = database.get_database()
 
     if not username_exists(username):
         return False
 
-    h1 = db.execute('SELECT * FROM user where username = ?', (username,)).fetchone()['password_hash']
+    h1 = db.execute('SELECT * FROM user WHERE username = ?', (username,)).fetchone()['password_hash']
     h2 = generate_password_hash(password)
 
     return h1 == h2
--- users.py
+++ users.py
@@ -56,26 +56,88 @@ def show_dashboard(username):
     return render_template('dashboard.html', username=username, desc=desc, datasets=datasets)
 
 
-@bp.route('/users/<username>/settings/')
-def show_settings(username):
-    if g.user is None or g.user['username'] != username:
+@bp.route('/settings/')
+def show_settings():
+    if g.user is None:
         abort(403)
-    return render_template('settings.html', userlist=list(auth_db.get_userlist()))
+    if auth_db.is_admin(g.user['username']):
+        return render_template('settings.html', userlist=list(auth_db.get_userlist()),
+                               invcode_list=list(auth_db.get_invcode_list()))
+    else:
+        return render_template('settings.html', userlist=[], invcode=[])
 
 
-@bp.route('/users/<username>/settings/edit_description', methods=('POST',))
-def edit_description(username):
-    if g.user is None or g.user['username'] != username:
+@bp.route('/settings/edit_description', methods=('POST',))
+def edit_description():
+    if g.user is None:
         abort(403)
-    auth_db.set_user_description(username, request.form['description'])
+    auth_db.set_user_description(g.user['username'], request.form['description'])
     return '', 204
 
 
-@bp.route('/users/<username>/settings/change_pw', methods=('POST',))
-def change_pw(username):
-    if g.user is None or g.user['username'] != username:
+@bp.route('/settings/change_pw', methods=('POST',))
+def change_pw():
+    if g.user is None:
         abort(403)
-    if request.form['password'] == '' or request.form['password'] != request.form['passwordAgain']:
-        return 'wrongPassword', 403
-    auth_db.set_user_password(g.user['username'], request.form['password'])
+    if not auth_db.validate_login(g.user['username'], request.form['passwordOld']):
+        return 'WRONG_OLD_PASSWORD', 403
+    if request.form['passwordNew'] == '' or request.form['passwordNew'] != request.form['passwordNewAgain']:
+        return 'PASSWORD_NO_MATCH', 403
+    auth_db.set_user_password(g.user['username'], request.form['passwordNew'])
     return '', 204
+
+
+# admin permission:
+
+@bp.route('/site_management/')
+def block_site_management():
+    abort(404)
+
+
+@bp.route('/site_management/set_admin', methods=('POST',))
+def set_admin():
+    if g.user is None or not auth_db.is_admin(g.user['username']):
+        return "PERMISSION_DENIED", 403
+    if auth_db.get_user(request.values['username']) is None:
+        return "USER_NOT_FOUND", 404
+    auth_db.set_admin(request.values['username'], not auth_db.is_admin(request.values['username']))
+    return "", 204
+
+
+@bp.route('/site_management/reset_password', methods=('POST',))
+def reset_password():
+    if g.user is None or not auth_db.is_admin(g.user['username']):
+        return jsonify({"text": "PERMISSION_DENIED", "password": ""})
+    if auth_db.get_user(request.values['username']) is None:
+        return jsonify({"text": "USER_NOT_FOUND", "password": ""})
+    pw = auth_db.generate_strong_password()
+    auth_db.set_user_password(request.values['username'], pw)
+    return jsonify({"text": "SUCCESS", "password": pw})
+
+
+@bp.route('/site_management/remove_user', methods=('POST',))
+def remove_user():
+    if g.user is None or not auth_db.is_admin(g.user['username']):
+        return "PERMISSION_DENIED", 403
+    if auth_db.get_user(request.values['username']) is None:
+        return "USER_NOT_FOUND", 404
+    auth_db.remove_user(request.values['username'])
+    return "", 204
+
+
+@bp.route('/site_management/append_invite_code', methods=('POST',))
+def append_invite_code():
+    if g.user is None or not auth_db.is_admin(g.user['username']):
+        return jsonify({"text": "PERMISSION_DENIED", "invcode": {}})
+    cid, code = auth_db.append_invite_code()
+    return jsonify({"text": "SUCCESS", "invcode": {'id': cid, 'code': code}})
+
+
+@bp.route('/site_management/remove_invite_code', methods=('POST',))
+def remove_invite_code():
+    if g.user is None or not auth_db.is_admin(g.user['username']):
+        return "PERMISSION_DENIED", 403
+    if not auth_db.invite_code_exists(request.values['code']):
+        return "CODE_NOT_FOUND", 404
+    auth_db.remove_invite_code(request.values['code'])
+    return "", 204
