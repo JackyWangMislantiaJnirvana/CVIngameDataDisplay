d0c62f5fa979a5eda41387ff7c7216ff4f2ac1e6
JackyWangMislantiaJnirvana <wmjwld@live.cn>
2019-06-25 17:14:28 +0800

    [refactor] clientDeployTool.lua

--- client/tools/clientDeployTool.lua
+++ client/tools/clientDeployTool.lua
@@ -1,21 +1,32 @@
 local os = require("os")
 local shell = require("shell")
+local fs = require("filesystem")
 
 local args = shell.parse(...)
 
 local urlHead = "https://raw.githubusercontent.com/JackyWangMislantiaJnirvana/CVIngameDataDisplay/client/client/"
 
+local function downloadFile(origin, destination)
+  print("Deploying " .. destination .. ".")
+  if fs.exists(destination) then
+    print("File already exists. Updating.")
+    os.execute("rm " .. destination)
+  end
+  os.execute("wget " .. urlHead .. origin .. " " .. destination)
+end
+
 -- lib deploy
-os.execute("rm /usr/lib/json.lua")
-os.execute("wget " .. urlHead .. "lib/json.lua" .. " /usr/lib/json.lua")
-os.execute("rm /usr/lib/logging.lua")
-os.execute("wget " .. urlHead .. "lib/logging.lua" .. " /usr/lib/logging.lua")
+print("Deploying /usr/lib/json.lua")
+downloadFile("lib/json.lua", "/usr/lib/json.lua")
+
+print("Deploying /usr/lib/logging.lua")
+downloadFile("lib/logging.lua", "/usr/lib/logging.lua")
 
 -- binary deploy
 if args[1] == "central" then
-  os.execute("rm /home/CVDDCentral.lua")
-  os.execute("wget " .. urlHead .. "src/CVDDCentral.lua" .. " /home/CVDDCentral.lua")
+  print("Deploying /home/CVDDCentral.lua")
+  downloadFile("src/CVDDCentral.lua", "/home/CVDDCentral.lua")
 elseif args[1] == "central" then
-  os.execute("rm /usr/lib/CVDDDataProvider.lua")
-  os.execute("wget " .. urlHead .. "src/CVDDDataProvider.lua" .. " /usr/lib/CVDDDataProvider.lua")
+  print("Deploying /usr/lib/CVDDDataProvider.lua")
+  downloadFile("src/CVDDDataProvider.lua", "/usr/lib/CVDDDataProvider.lua")
 end
