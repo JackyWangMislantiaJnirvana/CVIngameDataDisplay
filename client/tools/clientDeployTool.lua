cc8fdcd3350fba0d56d506f0b0bff2fc4ffa937b
JackyWangMislantiaJnirvana <wmjwld@live.cn>
2019-06-25 16:54:42 +0800

    [fix] deploy tool will delete target file before downloading the new one

--- client/tools/clientDeployTool.lua
+++ client/tools/clientDeployTool.lua
@@ -6,12 +6,16 @@ local args = shell.parse(...)
 local urlHead = "https://raw.githubusercontent.com/JackyWangMislantiaJnirvana/CVIngameDataDisplay/client/client/"
 
 -- lib deploy
+os.execute("rm /usr/lib/json.lua")
 os.execute("wget " .. urlHead .. "lib/json.lua" .. " /usr/lib/json.lua")
+os.execute("rm /usr/lib/logging.lua")
 os.execute("wget " .. urlHead .. "lib/logging.lua" .. " /usr/lib/logging.lua")
 
 -- binary deploy
 if args[1] == "central" then
+  os.execute("rm /home/CVDDCentral.lua")
   os.execute("wget " .. urlHead .. "src/CVDDCentral.lua" .. " /home/CVDDCentral.lua")
 elseif args[1] == "central" then
+  os.execute("rm /usr/lib/CVDDDataProvider.lua")
   os.execute("wget " .. urlHead .. "src/CVDDDataProvider.lua" .. " /usr/lib/CVDDDataProvider.lua")
 end
