de3a701b5e24904d6c1bb57a59b1046cb3482e6d
panda2134 <smallpanda2134@gmail.com>
2019-07-04 17:22:55 +0800

    delete client in server branch
    Signed-off-by: panda2134 <smallpanda2134@gmail.com>

--- client/lib/utils/seperate.lua
+++
@@ -1,8 +0,0 @@
-local function seperate(str, seperator)
-  local result = {}
-  string.gsub(str, "[^"..seperator.."]+",
-  function(w) table.insert(result, w) end)
-  return result
-end
-
-return seperate
